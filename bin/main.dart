4d1ef6ca626283887ffe4c9c4471c1a2c2840792
Kai Page <kaibug@gmail.com>
2019-08-10 19:46:49 -0500

    Init commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:onedrive_mass_upload/onedrive_mass_upload.dart' as onedrive_mass_upload;
+
+main(List<String> arguments) {
+  print('Hello world: ${onedrive_mass_upload.calculate()}!');
+}
